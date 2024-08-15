f6fe9daa34032dfc3fbc5631d6a72f2bdad847b8
hadilda19 <daddahadil01@gmail.com>
2024-08-14 20:04:26 -0400

    Fist Commit
    Date : 12-04-2022

---
+++ frmEmploye.Designer.cs
@@ -0,0 +1,380 @@
+﻿
+namespace prjFinalPOO
+{
+    partial class frmEmploye
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbltitre = new System.Windows.Forms.Label();
+            this.grpGestion = new System.Windows.Forms.GroupBox();
+            this.lblTotalEmp = new System.Windows.Forms.Label();
+            this.btnDernier = new System.Windows.Forms.Button();
+            this.btnSuivant = new System.Windows.Forms.Button();
+            this.btnPrecede = new System.Windows.Forms.Button();
+            this.btnPremier = new System.Windows.Forms.Button();
+            this.btnAnnuler = new System.Windows.Forms.Button();
+            this.btnSauvegarder = new System.Windows.Forms.Button();
+            this.btnSuppr = new System.Windows.Forms.Button();
+            this.btnModifier = new System.Windows.Forms.Button();
+            this.btnAjouter = new System.Windows.Forms.Button();
+            this.txtFonction = new System.Windows.Forms.TextBox();
+            this.txtSalaire = new System.Windows.Forms.TextBox();
+            this.txtNom = new System.Windows.Forms.TextBox();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.lblS = new System.Windows.Forms.Label();
+            this.lblF = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblNum = new System.Windows.Forms.Label();
+            this.grpRecherche = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblSalaire = new System.Windows.Forms.Label();
+            this.lblFonction = new System.Windows.Forms.Label();
+            this.lblNom = new System.Windows.Forms.Label();
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.grpGestion.SuspendLayout();
+            this.grpRecherche.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbltitre
+            // 
+            this.lbltitre.Font = new System.Drawing.Font("Andalus", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltitre.Location = new System.Drawing.Point(71, 20);
+            this.lbltitre.Name = "lbltitre";
+            this.lbltitre.Size = new System.Drawing.Size(638, 53);
+            this.lbltitre.TabIndex = 0;
+            this.lbltitre.Text = "EVALUATION SOMMATIVE DE FIN DE SESSION";
+            // 
+            // grpGestion
+            // 
+            this.grpGestion.Controls.Add(this.lblTotalEmp);
+            this.grpGestion.Controls.Add(this.btnDernier);
+            this.grpGestion.Controls.Add(this.btnSuivant);
+            this.grpGestion.Controls.Add(this.btnPrecede);
+            this.grpGestion.Controls.Add(this.btnPremier);
+            this.grpGestion.Controls.Add(this.btnAnnuler);
+            this.grpGestion.Controls.Add(this.btnSauvegarder);
+            this.grpGestion.Controls.Add(this.btnSuppr);
+            this.grpGestion.Controls.Add(this.btnModifier);
+            this.grpGestion.Controls.Add(this.btnAjouter);
+            this.grpGestion.Controls.Add(this.txtFonction);
+            this.grpGestion.Controls.Add(this.txtSalaire);
+            this.grpGestion.Controls.Add(this.txtNom);
+            this.grpGestion.Controls.Add(this.txtNum);
+            this.grpGestion.Controls.Add(this.lblS);
+            this.grpGestion.Controls.Add(this.lblF);
+            this.grpGestion.Controls.Add(this.label2);
+            this.grpGestion.Controls.Add(this.lblNum);
+            this.grpGestion.Location = new System.Drawing.Point(49, 76);
+            this.grpGestion.Name = "grpGestion";
+            this.grpGestion.Size = new System.Drawing.Size(714, 245);
+            this.grpGestion.TabIndex = 1;
+            this.grpGestion.TabStop = false;
+            this.grpGestion.Text = "Gestion Des employes De Microsoft";
+            // 
+            // lblTotalEmp
+            // 
+            this.lblTotalEmp.Location = new System.Drawing.Point(83, 209);
+            this.lblTotalEmp.Name = "lblTotalEmp";
+            this.lblTotalEmp.Size = new System.Drawing.Size(271, 23);
+            this.lblTotalEmp.TabIndex = 17;
+            // 
+            // btnDernier
+            // 
+            this.btnDernier.Location = new System.Drawing.Point(306, 165);
+            this.btnDernier.Name = "btnDernier";
+            this.btnDernier.Size = new System.Drawing.Size(94, 33);
+            this.btnDernier.TabIndex = 16;
+            this.btnDernier.Text = "Dernier";
+            this.btnDernier.UseVisualStyleBackColor = true;
+            // 
+            // btnSuivant
+            // 
+            this.btnSuivant.Location = new System.Drawing.Point(206, 165);
+            this.btnSuivant.Name = "btnSuivant";
+            this.btnSuivant.Size = new System.Drawing.Size(94, 33);
+            this.btnSuivant.TabIndex = 15;
+            this.btnSuivant.Text = "Suivant";
+            this.btnSuivant.UseVisualStyleBackColor = true;
+            // 
+            // btnPrecede
+            // 
+            this.btnPrecede.Location = new System.Drawing.Point(106, 165);
+            this.btnPrecede.Name = "btnPrecede";
+            this.btnPrecede.Size = new System.Drawing.Size(94, 33);
+            this.btnPrecede.TabIndex = 14;
+            this.btnPrecede.Text = "Precede";
+            this.btnPrecede.UseVisualStyleBackColor = true;
+            // 
+            // btnPremier
+            // 
+            this.btnPremier.Location = new System.Drawing.Point(6, 165);
+            this.btnPremier.Name = "btnPremier";
+            this.btnPremier.Size = new System.Drawing.Size(94, 33);
+            this.btnPremier.TabIndex = 13;
+            this.btnPremier.Text = "Premier";
+            this.btnPremier.UseVisualStyleBackColor = true;
+            // 
+            // btnAnnuler
+            // 
+            this.btnAnnuler.Location = new System.Drawing.Point(471, 199);
+            this.btnAnnuler.Name = "btnAnnuler";
+            this.btnAnnuler.Size = new System.Drawing.Size(129, 33);
+            this.btnAnnuler.TabIndex = 12;
+            this.btnAnnuler.Text = "Annuler";
+            this.btnAnnuler.UseVisualStyleBackColor = true;
+            // 
+            // btnSauvegarder
+            // 
+            this.btnSauvegarder.Location = new System.Drawing.Point(471, 165);
+            this.btnSauvegarder.Name = "btnSauvegarder";
+            this.btnSauvegarder.Size = new System.Drawing.Size(129, 33);
+            this.btnSauvegarder.TabIndex = 11;
+            this.btnSauvegarder.Text = "Sauvegarder";
+            this.btnSauvegarder.UseVisualStyleBackColor = true;
+            // 
+            // btnSuppr
+            // 
+            this.btnSuppr.Location = new System.Drawing.Point(471, 107);
+            this.btnSuppr.Name = "btnSuppr";
+            this.btnSuppr.Size = new System.Drawing.Size(129, 33);
+            this.btnSuppr.TabIndex = 10;
+            this.btnSuppr.Text = "Supprimer";
+            this.btnSuppr.UseVisualStyleBackColor = true;
+            this.btnSuppr.Click += new System.EventHandler(this.btnSuppr_Click);
+            // 
+            // btnModifier
+            // 
+            this.btnModifier.Location = new System.Drawing.Point(471, 68);
+            this.btnModifier.Name = "btnModifier";
+            this.btnModifier.Size = new System.Drawing.Size(129, 33);
+            this.btnModifier.TabIndex = 9;
+            this.btnModifier.Text = "Modifier";
+            this.btnModifier.UseVisualStyleBackColor = true;
+            // 
+            // btnAjouter
+            // 
+            this.btnAjouter.Location = new System.Drawing.Point(471, 27);
+            this.btnAjouter.Name = "btnAjouter";
+            this.btnAjouter.Size = new System.Drawing.Size(129, 33);
+            this.btnAjouter.TabIndex = 8;
+            this.btnAjouter.Text = "Ajouter";
+            this.btnAjouter.UseVisualStyleBackColor = true;
+            this.btnAjouter.Click += new System.EventHandler(this.btnAjouter_Click);
+            // 
+            // txtFonction
+            // 
+            this.txtFonction.Location = new System.Drawing.Point(161, 106);
+            this.txtFonction.Name = "txtFonction";
+            this.txtFonction.Size = new System.Drawing.Size(244, 22);
+            this.txtFonction.TabIndex = 7;
+            // 
+            // txtSalaire
+            // 
+            this.txtSalaire.Location = new System.Drawing.Point(161, 132);
+            this.txtSalaire.Name = "txtSalaire";
+            this.txtSalaire.Size = new System.Drawing.Size(244, 22);
+            this.txtSalaire.TabIndex = 6;
+            // 
+            // txtNom
+            // 
+            this.txtNom.Location = new System.Drawing.Point(161, 79);
+            this.txtNom.Name = "txtNom";
+            this.txtNom.Size = new System.Drawing.Size(244, 22);
+            this.txtNom.TabIndex = 5;
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(161, 51);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(244, 22);
+            this.txtNum.TabIndex = 4;
+            // 
+            // lblS
+            // 
+            this.lblS.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblS.Location = new System.Drawing.Point(27, 133);
+            this.lblS.Name = "lblS";
+            this.lblS.Size = new System.Drawing.Size(128, 27);
+            this.lblS.TabIndex = 3;
+            this.lblS.Text = "Salaire:";
+            // 
+            // lblF
+            // 
+            this.lblF.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblF.Location = new System.Drawing.Point(27, 106);
+            this.lblF.Name = "lblF";
+            this.lblF.Size = new System.Drawing.Size(128, 27);
+            this.lblF.TabIndex = 2;
+            this.lblF.Text = "Fonction:";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nom:";
+            // 
+            // lblNum
+            // 
+            this.lblNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNum.Location = new System.Drawing.Point(27, 52);
+            this.lblNum.Name = "lblNum";
+            this.lblNum.Size = new System.Drawing.Size(128, 27);
+            this.lblNum.TabIndex = 0;
+            this.lblNum.Text = "Numero";
+            // 
+            // grpRecherche
+            // 
+            this.grpRecherche.Controls.Add(this.groupBox1);
+            this.grpRecherche.Controls.Add(this.label1);
+            this.grpRecherche.Controls.Add(this.listBox1);
+            this.grpRecherche.Location = new System.Drawing.Point(49, 327);
+            this.grpRecherche.Name = "grpRecherche";
+            this.grpRecherche.Size = new System.Drawing.Size(702, 241);
+            this.grpRecherche.TabIndex = 2;
+            this.grpRecherche.TabStop = false;
+            this.grpRecherche.Text = "Recherche Employé Par Son Numero";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblSalaire);
+            this.groupBox1.Controls.Add(this.lblFonction);
+            this.groupBox1.Controls.Add(this.lblNom);
+            this.groupBox1.Controls.Add(this.lblNumero);
+            this.groupBox1.Location = new System.Drawing.Point(306, 57);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(373, 162);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Informations de Employé Selectionné";
+            // 
+            // lblSalaire
+            // 
+            this.lblSalaire.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSalaire.Location = new System.Drawing.Point(29, 131);
+            this.lblSalaire.Name = "lblSalaire";
+            this.lblSalaire.Size = new System.Drawing.Size(265, 31);
+            this.lblSalaire.TabIndex = 3;
+            // 
+            // lblFonction
+            // 
+            this.lblFonction.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblFonction.Location = new System.Drawing.Point(29, 95);
+            this.lblFonction.Name = "lblFonction";
+            this.lblFonction.Size = new System.Drawing.Size(265, 28);
+            this.lblFonction.TabIndex = 2;
+            // 
+            // lblNom
+            // 
+            this.lblNom.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblNom.Location = new System.Drawing.Point(29, 67);
+            this.lblNom.Name = "lblNom";
+            this.lblNom.Size = new System.Drawing.Size(265, 28);
+            this.lblNom.TabIndex = 1;
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblNumero.Location = new System.Drawing.Point(29, 27);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(265, 28);
+            this.lblNumero.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(39, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Choisir un numero";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(37, 55);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(162, 164);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // frmEmploye
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 571);
+            this.Controls.Add(this.grpRecherche);
+            this.Controls.Add(this.grpGestion);
+            this.Controls.Add(this.lbltitre);
+            this.Name = "frmEmploye";
+            this.Text = "frmEmploye";
+            this.Load += new System.EventHandler(this.frmEmploye_Load);
+            this.grpGestion.ResumeLayout(false);
+            this.grpGestion.PerformLayout();
+            this.grpRecherche.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbltitre;
+        private System.Windows.Forms.GroupBox grpGestion;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Label lblS;
+        private System.Windows.Forms.Label lblF;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblNum;
+        private System.Windows.Forms.GroupBox grpRecherche;
+        private System.Windows.Forms.TextBox txtFonction;
+        private System.Windows.Forms.TextBox txtSalaire;
+        private System.Windows.Forms.TextBox txtNom;
+        private System.Windows.Forms.Button btnAjouter;
+        private System.Windows.Forms.Label lblTotalEmp;
+        private System.Windows.Forms.Button btnDernier;
+        private System.Windows.Forms.Button btnSuivant;
+        private System.Windows.Forms.Button btnPrecede;
+        private System.Windows.Forms.Button btnPremier;
+        private System.Windows.Forms.Button btnAnnuler;
+        private System.Windows.Forms.Button btnSauvegarder;
+        private System.Windows.Forms.Button btnSuppr;
+        private System.Windows.Forms.Button btnModifier;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label lblNumero;
+        private System.Windows.Forms.Label lblSalaire;
+        private System.Windows.Forms.Label lblFonction;
+        private System.Windows.Forms.Label lblNom;
+    }
+}
---
+++ frmEmploye.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace prjFinalPOO
+{
+    public partial class frmEmploye : Form
+    {
+        public frmEmploye()
+        {
+            InitializeComponent();
+        }
+        struct Employe
+        {
+            public string Numero;
+            public string Nom;
+            public string Fonction;
+            public Int32 Salaire;
+        }
+        List<Employe> tabEmplyoyes = new List<Employe>();
+
+        private void frmEmploye_Load(object sender, EventArgs e)
+        {
+            StreamReader eFichier = new StreamReader("Compagnie.txt");
+            while (eFichier.EndOfStream == false)
+            {
+                Employe unemp;
+                unemp.Numero = eFichier.ReadLine();
+                unemp.Nom = eFichier.ReadLine();
+                unemp.Fonction = eFichier.ReadLine();
+                unemp.Salaire = Convert.ToInt32(eFichier.ReadLine());
+                tabEmplyoyes.Add(unemp);
+            }
+            eFichier.Close();
+            foreach (Employe q in tabEmplyoyes)
+            {
+
+                listBox1.Items.Add(q.Numero);
+            }
+
+        }
+
+        private void btnAjouter_Click(object sender, EventArgs e)
+        {
+
+            Employe un;
+            un.Numero = txtNum.Text;
+            un.Nom = txtNom.Text;
+            un.Salaire = Convert.ToInt32(txtSalaire.Text);
+            un.Fonction = txtFonction.Text;
+            tabEmplyoyes.Add(un);
+            listBox1.Items.Add(un.Numero);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string empchoisi = listBox1.SelectedItem.ToString();
+            for (int i = 0; i < tabEmplyoyes.Count; i++)
+            {
+                if (empchoisi == tabEmplyoyes[i].Numero)
+                {
+                    lblNumero.Text = tabEmplyoyes[i].Numero;
+                    lblNom.Text = tabEmplyoyes[i].Nom;
+                    lblFonction.Text = tabEmplyoyes[i].Fonction;
+                    lblSalaire.Text = tabEmplyoyes[i].Salaire.ToString();
+                }
+            }
+        }
+
+        private void btnSuppr_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+        }
+    }
+}
